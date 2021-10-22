4e7d22bf654f5adb6555426a331818e72dff23d9
kittenbothk <59426624+kittenbothk@users.noreply.github.com>
2021-10-22 13:10:33 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -12,6 +12,13 @@ let co2now = 0
 	//% blockId=cali block="Calibrate, Pin %pin"
 	//% group="Air Quality Sensor" weight=120
 	export function cali(pin: AnalogPin): number{
+		co2now = 0
+		for(let i = 0; i < 10; i++) {
+			co2now += pins.analogReadPin(pin)
+			basic.pause(200)
+		}
+		let co2raw = co2now / 10
+		co2zero=Math.abs(co2raw-400)
 		return co2zero
 	}
 
